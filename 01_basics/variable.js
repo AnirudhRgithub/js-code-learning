b04a465a1a1da8c2b8c42e158f4267d2100b163f
AnirudhRgithub <106009889+AnirudhRgithub@users.noreply.github.com>
2023-10-14 06:34:51 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,6 @@
+const accountId = 445516
+let accountEmail = "anirudh@gmail.com"
+var accountPassword = "123456"
+accountCity = "jaipur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
